7b73901c914790dbef6746aa3ceab147e364cd25
Brian Beard <brian.a.beard@gmail.com>
2019-06-18 18:57:26 -0500

    thumbnail & serve images

--- src/server/router.php
+++ src/server/router.php
@@ -4,45 +4,57 @@ require_once __DIR__ . '/thumb.php';
 
 $klein = new \Klein\Klein();
 
-// Since we are not using php's builtin server to serve static assets,
-// we need to handle that here. Otherwise in production, 
-// let nginx or apache handle it.
+// Note: Since we are not using php's builtin server to serve static assets,
+// we need to handle that here. Otherwise in production, we would let
+// nginx or apache handle it.
 $klein->respond('GET', '/?[:asset]?', function($request, $response) {
-    if (!$request->asset) {
-        readfile('./public/index.html');
-    } else {
-        $filename = './public/' . $request->asset;
-        $mime = mime_content_type($filename);
-        $response->header('Content-Type', $mime);
-        $response->sendHeaders();
-        readfile($filename);
-    }
+  if (!$request->asset) {
+    readfile('./public/index.html');
+  } else {
+    $filename = './public/' . $request->asset;
+
+    $mime = mime_content_type($filename);
+    $response->header('Content-Type', $mime);
+    $response->sendHeaders();
+
+    readfile($filename);
+  }
+});
+
+$klein->respond('GET', '/images/[:size]/[:filename]', function($request, $response){
+  $f = __DIR__ . '/images/' . $request->size . '/' . $request->filename;
+
+  $mime = mime_content_type($f);
+  $response->header('Content-Type', $mime);
+  $response->sendHeaders();
+
+  readfile($f);
 });
 
 $klein->respond('POST', '/images', function($request, $response) {
 
-    $sizes = [ 
-        'small' => [300, 300],
-        'medium' => [600, 600],
-        'large' => [1000, 1000]
-    ];
+  $sizes = [
+    'small' => [300, 300],
+    'medium' => [600, 600],
+    'large' => [1000, 1000]
+  ];
 
-    $body = json_decode($request->body(), true);
+  $body = json_decode($request->body(), true);
 
-    if(!array_key_exists('remoteImage', $body)) {
-        $response->code(400); // Bad Request
-        return;
-    }
+  if(!array_key_exists('remoteImage', $body)) {
+    $response->code(400); // Bad Request
+    return;
+  }
 
-    $url = $body['remoteImage'];
-    try {
-        $results = thumbRemoteImage($url, $sizes);
-    } catch (Exception $e) {
-        $response->code(422); // Unprocessable Entity
-        return;
-    }
+  $url = $body['remoteImage'];
+  try {
+    $results = thumbRemoteImage($url, $sizes);
+  } catch (Exception $e) {
+    $response->code(422); // Unprocessable Entity
+    return;
+  }
 
-    $response->json($results);
+  $response->json($results);
 });
 
 $klein->dispatch();
--- src/server/thumb.php
+++ src/server/thumb.php
@@ -1,46 +1,74 @@
-<?php 
+<?php
 
 function thumbRemoteImage($url, $sizes) {
 
-  $imageFolder = __DIR__ . "/images/";
-
+  // Whitelist of accepted extensions
   $validExtensions = array(
     'image/jpeg' => 'jpg',
     'image/png' => 'png',
-    'image/gif' => 'gif', 
+    'image/gif' => 'gif',
   );
 
-  $mime = mime_content_type($url);
+  $headers = get_headers($url, 1);
 
+  // Check remote response was 200 and Content-Type exists
+  if(!strpos($headers[0], '200') || !array_key_exists('Content-Type', $headers)) {
+    throw new Exception('Cannot load url ' . $url);
+  }
+
+  $mime = $headers['Content-Type'];
+
+  // Only whitelisted file types
   if(!array_key_exists($mime, $validExtensions)) {
     throw new Exception('Cannot process file type ' . $mime);
   }
 
   $ext = $validExtensions[$mime];
 
-  // TODO: copy url contents
+  $contents = file_get_contents($url);
+
+  $img = imagecreatefromstring($contents);
+
+  // Original dimensions
+  $oWidth = imagesx($img);
+  $oHeight = imagesy($img);
 
   $result = [];
   foreach($sizes as $size => $dimensions) {
-    // TODO: resize the image to their respective sizes
+
+    $thumb = imagecreatetruecolor($dimensions[0], $dimensions[1]);
+
+    // Turn off alpha blending
+    imagealphablending($thumb, false);
+
+    imagecopyresized($thumb, $img, 0, 0, 0, 0, $dimensions[0], $dimensions[1], $oWidth, $oHeight);
+
+    // Hash url for filename in case of collision.
+    // Example `a.com/cat.jpg` <-> `b.com/cat.jpg` will collide
+    // if we save plain filename `cat.jpg` on fs.
+    $newurl = 'images/' . $size . '/' . md5($url) . '.' . $ext;
+    $save = __DIR__ . '/' . $newurl;
+
+    switch($ext) {
+      case 'jpg':
+        imagejpeg($thumb, $save);
+        break;
+      case 'png':
+        imagesavealpha($thumb, true);
+        imagepng($thumb, $save);
+        break;
+      case 'gif':
+        imagesavealpha($thumb, true);
+        imagegif($thumb, $save);
+        break;
+    }
+
+    imagedestroy($thumb);
+
+    $result[$size] = array('type' => $ext, 'url' => $newurl);
   }
 
-  $fakeResponse = array(
-      "small" => array(
-          "type" => $ext,
-          "url" => $url
-      ),
+  imagedestroy($img);
 
-      "medium" => array(
-          "type" => $ext,
-          "url" => $url
-      ),
-
-      "large" => array(
-          "type" => $ext,
-          "url" => $url
-      )
-  );
-
-  return $fakeResponse;
+  return $result;
 }
